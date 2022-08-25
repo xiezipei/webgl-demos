8eca0e0bb2440c0e38d9e7c97ef8f6c4ddb8a7fc
xiezipei <zipeixie@gmail.com>
2022-08-25 17:44:50 +0800

    feat(wpg): HelloPoint

--- wpg/demo-01/HelloCanvas.js
+++ wpg/demo-01/HelloCanvas.js
@@ -3,7 +3,7 @@ function main() {
   var canvas = document.getElementById('app');
 
   // 获取webgl绘图上下文
-  var gl = getWebGLContext(canvas); // 不引入库会报错
+  var gl = getWebGLContext(canvas); // 引入库写法（不引入库会报错找不到方法）
   // var gl = canvas.getContext('webgl');   // 不引入库写法
 
   if (!gl) {
@@ -14,6 +14,6 @@ function main() {
   // 指定清空<canvas>颜色
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-  // 清空<canvas>
+  // 清空<canvas>（理解：执行完清空操作后就会显示清空后的颜色）
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
---
+++ wpg/demo-01/HelloPoint.js
@@ -0,0 +1,41 @@
+// 顶点着色器
+const VSHADER_SOURCE = `
+    void main() {
+        gl_Position = vec4(0.0, 0.0, 0.0, 1.0); // 设置坐标
+        gl_PointSize = 10.0; // 设置尺寸
+    }
+`;
+
+// 片着色器
+const FSHADER_SOURCE = `
+    void main() {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // 设置颜色
+    }
+`;
+
+function main() {
+  // 获取<canvas>元素
+  const canvas = document.getElementById('app');
+
+  // 获取webgl绘图上下文
+  const gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // 初始化着色器
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to initialize shaders.');
+    return;
+  }
+
+  // 设置<canvas>的背景色
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  // 清空<canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // 绘制一个点
+  gl.drawArrays(gl.POINT, 0, 1);
+}
