b063c459222f88e3b161d6229d94bf48a5e093f3
xiezipei <zipeixie@gmail.com>
2022-08-30 14:32:47 +0800

    feat: update clickedpoint

--- wpg/demo-01/ClickedPoint.js
+++ wpg/demo-01/ClickedPoint.js
@@ -46,6 +46,11 @@ function main() {
     return;
   }
 
+  // 注册鼠标点击事件响应函数
+  canvas.onmousedown = function (ev) {
+    click(ev, gl, canvas, a_Position);
+  };
+
   // 将顶点位置传输给attribute变量
   gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
 
@@ -57,4 +62,45 @@ function main() {
 
   // 绘制一个点
   gl.drawArrays(gl.POINT, 0, 1);
+}
+
+/**
+ * 鼠标点击位置数组
+ */
+const g_points = [];
+
+/**
+ * canvas 点击事件
+ * @param {*} ev
+ * @param {*} gl
+ * @param {*} canvas
+ * @param {*} a_Position
+ */
+function click(ev, gl, canvas, a_Position) {
+  // 鼠标点击的 x 坐标
+  let x = ev.clientX;
+  // 鼠标点击的 y 坐标
+  let y = ev.clientY;
+
+  // ?
+  let rect = ev.target.getBoundingClientRect();
+  // ?
+  x = (x - rect.left - canvas.height / 2) / (canvas.height / 2);
+  // ?
+  y = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
+
+  // 将坐标存储到 g_points 数组中
+  g_points.push(x);
+  g_points.push(y);
+
+  // 清除 <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  let len = g_points.length;
+  for (let i = 0; i < len; i += 2) {
+    // 将点的位置传递到变量中 a_Position
+    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
+    // 绘制点
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
 }
