0b4fe139e65c871e362d5f95169d556da0304751
xiezipei <zipeixie@gmail.com>
2022-08-30 11:56:15 +0800

    feat: hellopoint2

--- wpg/demo-01/HelloPoint2.js
+++ wpg/demo-01/HelloPoint2.js
@@ -2,10 +2,11 @@
  * 顶点着色器（GLSL ES语言）
  */
 const VSHADER_SOURCE = `
-    void main() {
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0); // 设置坐标
-        gl_PointSize = 10.0; // 设置尺寸
-    }
+  attribute vec4 a_Position;
+  void main() {
+      gl_Position = a_Position; // 设置坐标
+      gl_PointSize = 10.0; // 设置尺寸
+  }
 `;
 
 /**
@@ -13,7 +14,7 @@ const VSHADER_SOURCE = `
  */
 const FSHADER_SOURCE = `
     void main() {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // 设置颜色
+        gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0); // 设置颜色
     }
 `;
 
@@ -37,6 +38,16 @@ function main() {
     console.log('Failed to initialize shaders.');
     return;
   }
+
+  // 获取attribute变量的存储位置
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return;
+  }
+
+  // 将顶点位置传输给attribute变量
+  gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
 
   // 设置<canvas>的背景色
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
